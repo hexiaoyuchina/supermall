716a4cc0ce5e49e57f05ca70eb531bdbc02d8159
hexiaoyu <1642567075@qq.com>
2021-01-23 15:56:05 +0800

    目录结构搭建

---
+++ src/vue.config.js
@@ -0,0 +1,15 @@
+module.exports={
+    configureWebpack:{
+        resolve:{
+            alias:{
+                'assets':'@/assets',
+                'common':'@/common',
+                'componets':'@/componets',
+                'network':'@/network',
+                'views':'@/views',
+           
+
+            }
+        }
+    }
+}
